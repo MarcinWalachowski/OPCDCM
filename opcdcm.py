e9b25971adf764a878479f4c8a767b129da43365
LAPTOP-AE3LKOPM\malci <malcinek@o2.pl>
2023-07-30 22:48:10 +0200

    [popup info] Popup info window added, no complex logic to set the text properly, Layout corrected, writelines to file modify to new dict

--- opcdcm.py
+++ opcdcm.py
@@ -3,7 +3,7 @@ from Variable import list_of_variable
 from VariableNaming import list_of_variable_naming
 from LogDataConf import list_log_data_conf
 
-
+error = 0
 lencolumn = 3
 lenrow = 22
 opcdcmdatainput = [[0 for i in range(lencolumn)] for i in range(lenrow)]
@@ -14,6 +14,7 @@ items3 = ("Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
 
 dpg.create_context()
 dpg.create_viewport(title='OPCDCM config generator', width=600, height=300)
+
 
 def myfunprint(sender, data, user_data):
     global opcdcmdatainput
@@ -40,6 +41,7 @@ def myfunprint(sender, data, user_data):
 def myfunfilegenerate(sender, data):
     global opcdcmdatainput
     print(f"Generacja plików")
+    global error
     with open('Initialize.txt', 'w') as f:
         f.write(f"//*********************************// \n")
         f.write(f"//*** Names for UserDataLogging ***// \n")
@@ -47,14 +49,17 @@ def myfunfilegenerate(sender, data):
         f.write(f"\n")
         for i in range(lenrow):
             if opcdcmdatainput[i][0] != 0:
-                f.writelines(f"// Naming for the UCI.Loop[{opcdcmdatainput[i][1]}].{list_of_variable[opcdcmdatainput[i][2]]} \n")
+                temp = list_of_variable[opcdcmdatainput[i][2]]
+                temp = temp.replace("?", opcdcmdatainput[i][1])
+                f.writelines(f"// Naming for the {temp} \n")
                 temp = opcdcmdatainput[i][0]
                 for k in range(16):
                     if k < len(temp):
                         if temp[k] != "?":
                             f.writelines({f"UserDataLoggingColumnName100ms[{i}].DATA[{k}] := {ord(temp[k])} \n"})
                         else:
-                            f.writelines({f"UserDataLoggingColumnName100ms[{i}].DATA[{k}] := {ord(opcdcmdatainput[i][1])} \n"})
+                            f.writelines(
+                                {f"UserDataLoggingColumnName100ms[{i}].DATA[{k}] := {ord(opcdcmdatainput[i][1])} \n"})
                     else:
                         f.writelines({f"UserDataLoggingColumnName100ms[{i}].DATA[{k}] := 32 \n"})
                 f.writelines({f"UserDataLoggingColumnName100ms[{i}].LEN := 16; \n\n"})
@@ -67,16 +72,18 @@ def myfunfilegenerate(sender, data):
         for i in range(lenrow):
             if opcdcmdatainput[i][0] != 0:
                 tempp = opcdcmdatainput[i][2]
+                temp = list_of_variable[opcdcmdatainput[i][2]]
+                temp = temp.replace("?", opcdcmdatainput[i][1])
                 if list_log_data_conf[tempp][0] == '0':
                     f.writelines(f"//----------------------------------------------------------------------------------------------------------------------------------- \n")
                     opcdcmdatainput[i][0] = opcdcmdatainput[i][0].replace("?", opcdcmdatainput[i][1])
                     f.writelines(f"//{opcdcmdatainput[i][0]} \n")
-                    f.writelines(f"UserDataLogBuffer100ms[{i}] := UCI.Loop[{opcdcmdatainput[i][1]}].{list_of_variable[opcdcmdatainput[i][2]]}; \n\n")
+                    f.writelines(f"UserDataLogBuffer100ms[{i}] := {temp}; \n\n")
                 else:
                     f.writelines(f"//----------------------------------------------------------------------------------------------------------------------------------- \n")
                     opcdcmdatainput[i][0] = opcdcmdatainput[i][0].replace("?", opcdcmdatainput[i][1])
                     f.writelines(f"//{opcdcmdatainput[i][0]} \n")
-                    f.writelines(f"If UCI.Loop[{opcdcmdatainput[i][1]}].{list_of_variable[opcdcmdatainput[i][2]]} Then\n")
+                    f.writelines(f"If {temp} Then\n")
                     #print(f" wiersz {i}")
                     #print(f" log_data_conf {list_log_data_conf[i][2]}")
                     f.writelines(f"\t UserDataLogBuffer100ms[{i}] := {list_log_data_conf[tempp][2]}; \n")
@@ -87,13 +94,10 @@ def myfunfilegenerate(sender, data):
                 f.writelines(f"//----------------------------------------------------------------------------------------------------------------------------------- \n")
                 f.writelines(f"//Not defined \n")
                 f.writelines(f"UserDataLogBuffer100ms[{i}] := 0; \n\n")
-
     f.close()
 
 
 with dpg.window(label="OPCDCM Configurator", tag="Primary Window"):
-    dpg.add_text("Test version")
-    dpg.add_button(label="Generate files", callback=myfunfilegenerate)
 
     with dpg.table(width=780, header_row=True, row_background=True,
                    borders_innerH=True, borders_outerH=True, borders_innerV=True,
@@ -101,7 +105,6 @@ with dpg.window(label="OPCDCM Configurator", tag="Primary Window"):
         dpg.add_table_column(label="Signal number")
         dpg.add_table_column(label="Signal tag", width=200)
         dpg.add_table_column(label="Loop No", width=200)
-        dpg.add_table_column(label="Header 3", width=200)
 
         for i in range(lenrow):
             with dpg.table_row():
@@ -110,10 +113,18 @@ with dpg.window(label="OPCDCM Configurator", tag="Primary Window"):
                               no_preview=False, popup_align_left=True, callback=myfunprint, user_data=[1, i])
                 dpg.add_combo(tag=221+i, items=items2, height_mode=dpg.mvComboHeight_Small, no_arrow_button=False,
                                          no_preview=False, popup_align_left=True, callback=myfunprint, user_data=[2, i])
-                #dpg.add_combo(tag=321+i, items=items3, height_mode=dpg.mvComboHeight_Small, no_arrow_button=False,
-                #                         no_preview=False, popup_align_left=True, callback=myfunprint, user_data=[3, i])
 
-dpg.create_viewport(title='Custom Title', width=790, height=500)
+    dpg.add_button(label="Generate files", tag="GenFile", pos=(280, 540), width=200, height=30, callback=myfunfilegenerate)
+    dpg.add_text("Test version", pos=(340, 590), color=[255, 0, 0])
+    with dpg.popup(parent="GenFile", tag="GenInfo", mousebutton=0, modal=True):
+        if error ==0:
+            dpg.add_text("Files generated with success")
+            dpg.add_button(label="Ok", callback=lambda: dpg.configure_item("GenInfo", show=False))
+        else:
+            dpg.add_text("Files generation failed")
+
+
+dpg.create_viewport(title='OPCDCM Configurator', width=790, height=660)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.set_primary_window("Primary Window", True)
